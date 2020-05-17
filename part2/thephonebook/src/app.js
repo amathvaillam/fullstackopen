8b9f0715f5bc06e3416a2b91467dee89327d168e
amath vaillam <amathv01@gmail.com>
2020-05-17 01:27:46 +0000

    thephonebook step11

--- part2/thephonebook/src/app.js
+++ part2/thephonebook/src/app.js
@@ -1,40 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import personServices from './services/persons'
+import './app.css'
 
-const Filter = ({toFilter, handleChangeFilter}) => (
-  <div>
-    filter shown with<input  value={toFilter} onChange={handleChangeFilter} />
-</div>
-)
+const Filter = ({toFilter, handleChangeFilter}) =>
+(<div>filter shown with<input  value={toFilter} onChange={handleChangeFilter} /></div>)
+
+const Notification = ({ message, classname }) => message === null
+? null
+: <div className={classname}>{message}</div>
 
 const PersonForm = ({handleSubmit, newName, handleChangeName, newNumber, handleChangeNumber}) => (
   <form onSubmit={handleSubmit}>
-    <div>
-      name: <input  value={newName} onChange={handleChangeName} />
-  </div>
-  <div>
-    number: <input  value={newNumber} onChange={handleChangeNumber} />
-</div>
-<div>
-  <button disabled={!newName.length || !newNumber.length}type="submit" >add</button>
-</div>
-</form>
+    <div>name: <input  value={newName} onChange={handleChangeName} /></div>
+    <div>number: <input  value={newNumber} onChange={handleChangeNumber} /></div>
+    <div><button disabled={!newName.length || !newNumber.length}type="submit" >add</button></div>
+  </form>
 )
 
 const Persons = ({shown, handleDelete}) =>
-{
-  return shown.map(({name, number, id}) => {
-    return (
-      <p key={id}>{name} {number}
-        <button onClick={(event) => {
-            if(window.confirm('Delete '+name+' ?'))
-            handleDelete(id)
-          }}>delete</button>
-        </p>
-      )
-    })
-  }
-
+shown.map(({name, number, id}) => {
+  return (
+    <p key={id}>{name} {number}
+      <button onClick={(event) => {
+          if(window.confirm('Delete '+name+' ?'))
+          handleDelete(id)
+        }}>delete</button>
+      </p>
+    )
+  })
 
   const App = () => {
     const [ persons, setPersons ] = useState([])
@@ -43,8 +36,20 @@ const Persons = ({shown, handleDelete}) =>
     const [ toFilter, setToFilter ] = useState('')
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNumber ] = useState('')
+    const [ message, setMessage ] = useState('')
+    const [ classname, setClassname ] = useState('')
 
-    const getAll = () => {personServices.getAll()
+    const success = (nature, name, number='') => {
+      let message = nature === 'name'
+      ? 'Added '+name
+      : `Changed number ${number} of ${name}`
+      setClassname('success')
+      setMessage(message)
+      window.setTimeout(() => {setMessage(null)},2000)
+    }
+
+    const getAll = () => {
+      personServices.getAll()
       .then(persons => {
         setPersons(persons)
         setShown(persons)
@@ -75,16 +80,22 @@ const Persons = ({shown, handleDelete}) =>
         with a new one?`)){
           let id = persons.filter((element) => element.name === newName)[0].id
           personServices.update(id, newObject)
-          .then(response => {getAll()})
+          .then(response => {
+            success('number',newName ,newNumber)
+            getAll()
+          })
           .catch(error => {
             console.log('fail')
           })
         }
         else{
           personServices.create(newObject)
-          .then(response => {getAll()})
+          .then(response => {
+            success('name',newName)
+            getAll()
+          })
           .catch(error => {
-            console.log('fail')
+            console.log(error)
           })
         }
       }
@@ -100,6 +111,7 @@ const Persons = ({shown, handleDelete}) =>
       return (
         <div>
           <h2>Phonebook</h2>
+          <Notification message={message} classname={classname}></Notification>
           <Filter toFilter={toFilter} handleChangeFilter={handleChangeFilter}></Filter>
           <h2>add a new</h2>
           <PersonForm
